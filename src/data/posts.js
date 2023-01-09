7da378563d3d8f28f523eef9b35258b17d478b6a
stap20 <kalponkalson@gmail.com>
2023-01-10 01:24:06 +0200

    finish post primary logic and like function only)

--- src/data/posts.js
+++ src/data/posts.js
@@ -4,15 +4,20 @@ const posts = [
     {
         id: 't1',
         user: users[0],
-        createdAt: '2022:12:04 12:00:00',
-        content: 'Lorem Ipsum is simply dummy text of the printing and ' +
-            'typesetting industry. Lorem Ipsum has been the ' +
-            'industry\'s standard dummy text ever ' +
-            'since the 1500s, when an unknown printer ' +
-            'took a galley of type and scrambled it to ' +
+        createdAt: '2023/01/09 23:32:20',
+        content: 'Lorem Ipsum is simply dummy text of the printing and \n' +
+            'typesetting industry. Lorem Ipsum has been the \n' +
+            'industry\'s standard dummy text ever \n' +
+            'since the 1500s, when an unknown printer \n' +
+            'took a galley of type and scrambled it to \n' +
+            'industry\'s standard dummy text ever \n' +
+            'since the 1500s, when an unknown printer \n' +
+            'took a galley of type and scrambled it to \n' +
+            'industry\'s standard dummy text ever \n' +
+            'since the 1500s, when an unknown printer \n' +
+            'took a galley of type and scrambled it to \n' +
             'make a type specimen book.',
         image: require('../../assets/image_1.png'),
-        isImage: true,
         numberOfComments: 126,
         numberOfShares: 506,
         numberOfLikes: 53,
@@ -20,12 +25,10 @@ const posts = [
     {
         id: 't2',
         user: users[1],
-        createdAt: '2022:12:01 2:03:14',
+        createdAt: '2022/11/01 12:03:14',
         content: 'Lorem Ipsum is simply dummy text of the printing and ' +
             'typesetting industry. Lorem Ipsum has been the ' +
             'make a type specimen book.',
-        image: null,
-        isImage: false,
         numberOfComments: 0,
         numberOfShares: 10,
         numberOfLikes: 3,
@@ -33,11 +36,10 @@ const posts = [
     {
         id: 't3',
         user: users[2],
-        createdAt: '2022:12:04 2:03:14',
+        createdAt: '2022/12/04 2:03:14',
         content: 'Lorem Ipsum is simply dummy text of the printing and ' +
             'typesetting industry. ',
         image: require('../../assets/image_2.png'),
-        isImage: true,
         numberOfComments: 260,
         numberOfShares: 623,
         numberOfLikes: 1025,
@@ -45,11 +47,10 @@ const posts = [
     {
         id: 't4',
         user: users[3],
-        createdAt: '2022:07:04 14:02:00',
+        createdAt: '2022/07/04 14:02:00',
         content: 'Lorem Ipsum is simply dummy text of the printing and ' +
             'typesetting industry. ',
-        image: require('../../assets/image_3.png'),
-        isImage: true,
+        image: require('../../assets/image_3.jpg'),
         numberOfComments: 126,
         numberOfShares: 506,
         numberOfLikes: 53,
--- src/screens/post/PostCard.js
+++ src/screens/post/PostCard.js
@@ -1,50 +1,159 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import UserAvatar from "@muhzi/react-native-user-avatar";
 import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
 import Ionicons from "react-native-vector-icons/Ionicons";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
+const month = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
+function calcTime(offset) {
+  // create Date object for current location
+  var d = new Date();
+
+  // convert to msec
+  // subtract local time zone offset
+  // get UTC time in msec
+  var utc = d.getTime() + d.getTimezoneOffset() * 60000;
+
+  // create new Date object for different city
+  // using supplied offset
+  var nd = new Date(utc + 3600000 * offset);
+
+  // return time as a string
+  return nd.toLocaleString();
+}
+
+function getPostDuration(postDate) {
+  // var postDate = new Date(post.createdAt).getTime();
+  // var currDate = new Date().getTime();
+  // var postDuration = new Date(currDate - postDate);
+
+  var postDate = new Date(postDate).getTime();
+  var currDate = new Date(calcTime("+2")).getTime();
+  var duration = currDate - postDate;
+
+  var days = Math.floor(duration / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((duration % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((duration % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((duration % (1000 * 60)) / 1000);
+
+  var durationString = "";
+
+  if (days > 90) {
+    var date = new Date(postDate);
+
+    durationString =
+      date.getDate() +
+      " " +
+      month[date.getMonth()] +
+      " " +
+      date.getFullYear().toString().substr(-2);
+  } else if (days > 30 && days <= 90) {
+    var date = new Date(postDate);
+    durationString = date.getDate() + " " + month[date.getMonth()];
+  } else if (days !== 0 && days > 0) {
+    durationString = days + " d";
+  } else if (hours !== 0 && hours > 0) {
+    durationString = hours + " h";
+  } else if (minutes !== 0 && minutes > 0) {
+    durationString = minutes + " m";
+  } else if (seconds !== 0 && seconds > 0) {
+    durationString = seconds + " s";
+  } else {
+    durationString = "Just now";
+  }
+
+  return durationString;
+  // console.log(postDuration)
+}
+
 function ActionButton(props) {
   let CurrentIcon;
 
   switch (props.type) {
     case "share":
-      CurrentIcon = <FontAwesome5 size={30} name={"share"} color={"white"} />;
+      CurrentIcon = <FontAwesome5 size={30} name={"share"} color={"#660032"} />;
       break;
-    case "like":
-      CurrentIcon = <Ionicons size={30} name={"heart"} color={"white"} />;
+    case "not-liked":
+      CurrentIcon = (
+        <Ionicons size={30} name={"heart-outline"} color={"#660032"} />
+      );
+      break;
+    case "liked":
+      CurrentIcon = <Ionicons size={30} name={"heart"} color={"#660032"} />;
       break;
     case "comment":
-      CurrentIcon = <Ionicons size={30} name={"chatbubbles"} color={"white"} />;
+      CurrentIcon = (
+        <Ionicons size={30} name={"chatbubbles"} color={"#660032"} />
+      );
       break;
   }
 
   return (
-    <TouchableOpacity style={actionBtnStyles.container}>
+    <TouchableOpacity style={actionBtnStyles.container} onPress={props.onPress}>
       {CurrentIcon}
-      
+      <Text style={actionBtnStyles.actionText}>{props.details}</Text>
     </TouchableOpacity>
   );
 }
 
 export default function PostCard(props) {
-  var numLines = 3;
-  const [postDetailsLines, setLines] = useState(numLines);
+  let { post } = props;
 
-  let { post, isFound = false, isLost = false } = props;
-  let postImage = post.user.profileImage;
+  const [numOfLikes, setNumOfLikes] = useState(post.numberOfLikes); //To show ur remaining Text
+  const [isLiked, setIsLiked] = useState(false);
+  const [textShown, setTextShown] = useState(false); //To show ur remaining Text
+  const [lengthMore, setLengthMore] = useState(false); //to show the "Read more & Less Line"
+  const toggleNumberOfLines = () => {
+    //To toggle the show text or hide it
+    setTextShown(!textShown);
+  };
+
+  const onTextLayout = useCallback((e) => {
+    setLengthMore(e.nativeEvent.lines.length >= 4); //to check the text is more than 4 lines or not
+  }, []);
+
+  const onMakeLike = () => {
+    if (isLiked) {
+      setNumOfLikes(numOfLikes - 1);
+    } else {
+      setNumOfLikes(numOfLikes + 1);
+    }
+
+    setIsLiked(!isLiked);
+  };
+
+  var postDuration = getPostDuration(post.createdAt);
+  var imageHeight;
+
+  if (post.hasOwnProperty("image") == true) {
+    imageHeight = Image.resolveAssetSource(post.image).height;
+  }
 
   return (
     <View style={styles.container}>
       <View style={headerStyles.container}>
         <View style={headerStyles.avatarContainer}>
-          <UserAvatar size={35} userName="Josh Samuel" fontSize={15} />
+          <UserAvatar size={35} src={post.user.profileImage} fontSize={15} />
         </View>
         <View style={headerStyles.headerDetailsContainer}>
-          <Text style={headerStyles.userName}>{"Josh Samuel"}</Text>
+          <Text style={headerStyles.userName}>{post.user.name}</Text>
 
-          <Text style={headerStyles.postTime}>{"18m"}</Text>
+          <Text style={headerStyles.postTime}>{postDuration}</Text>
         </View>
         <TouchableOpacity style={headerStyles.headerDotsContainer}>
           <MaterialCommunityIcons
@@ -55,35 +164,45 @@ export default function PostCard(props) {
         </TouchableOpacity>
       </View>
 
-      <View style={styles.detailsContainer}>
-        <Text style={styles.detailsText} numberOfLines={postDetailsLines}>
-          {
-            "Hi i found something in AllRayyen\nit is an apple watch series 6 red color"
-          }
+      <TouchableOpacity
+        style={styles.detailsContainer}
+        onPress={toggleNumberOfLines}
+      >
+        <Text
+          style={styles.detailsText}
+          onTextLayout={onTextLayout}
+          numberOfLines={textShown ? undefined : 4}
+        >
+          {post.content}
         </Text>
-        {/* {postDetailsLines === 0 ? (
-          <Text
-            onPress={() => setLines(numLines)}
-            style={{ fontWeight: "bold" }}
-          >
-            Read Less
-          </Text>
-        ) : (
-          <Text onPress={() => setLines(0)} style={{ fontWeight: "bold" }}>
-            Read More
-          </Text>
-        )} */}
-      </View>
 
-      <View style={styles.imageContainer}>
-        <Image
-          style={styles.postImage}
-          source={require("../../../assets/image_2_f.png")}
-        />
-      </View>
+        {lengthMore && !textShown ? (
+          <TouchableOpacity onPress={toggleNumberOfLines}>
+            <Text style={[styles.detailsText, { fontWeight: "bold" }]}>
+              {"Read more"}
+            </Text>
+          </TouchableOpacity>
+        ) : null}
+      </TouchableOpacity>
+      {post.hasOwnProperty("image") == true && (
+        <View
+          style={[
+            styles.imageContainer,
+            { height: imageHeight >= 450 ? 450 : imageHeight },
+          ]}
+        >
+          <Image style={styles.postImage} source={post.image} />
+        </View>
+      )}
 
       <View style={styles.actionContainer}>
-        <ActionButton type={"like"}/>
+        <ActionButton
+          type={isLiked ? "liked" : "not-liked"}
+          details={numOfLikes}
+          onPress={onMakeLike}
+        />
+        <ActionButton type={"comment"} details={post.numberOfComments} />
+        <ActionButton type={"share"} details={post.numberOfShares} />
       </View>
     </View>
   );
@@ -135,30 +254,33 @@ const actionBtnStyles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "red",
+  },
+
+  actionText: {
+    fontSize: 12,
+    color: "#660032",
+    marginLeft: 10,
   },
 });
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flexShrink: 1,
     backgroundColor: "white",
-    height: 450,
     marginBottom: 17,
     paddingVertical: 10,
   },
 
   detailsContainer: {
-    flex: 1,
+    flexShrink: 1,
     marginBottom: 10,
-    minHeight: 20,
-    maxHeight: 40,
     marginHorizontal: 10,
     paddingLeft: 10,
   },
 
   imageContainer: {
-    flex: 1,
+    flexShrink: 1,
+    flexGrow: 1,
     marginBottom: 10,
     marginHorizontal: 10,
     borderRadius: 15,
@@ -169,10 +291,7 @@ const styles = StyleSheet.create({
     flex: 1,
     minHeight: 40,
     maxHeight: 40,
-    backgroundColor: "blue",
     flexDirection: "row",
-    justifyContent: "space-between",
-    marginHorizontal: 10,
   },
 
   postImage: {
@@ -184,8 +303,9 @@ const styles = StyleSheet.create({
   },
 
   detailsText: {
-    fontSize: 14,
+    fontSize: 13,
     color: "#660032",
+    lineHeight: 18,
   },
 });
 
