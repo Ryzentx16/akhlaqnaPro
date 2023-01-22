d9d6efc19db68d57864f57015a1acf9798c47a1b
stap20 <kalponkalson@gmail.com>
2023-01-23 00:21:36 +0200

    final fixed version

--- App.js
+++ App.js
@@ -90,7 +90,7 @@ export default function App() {
       })
       .then((res) => {
         if (res) {
-          console.log("a7awy");
+          console.log("donawy");
         } else {
         }
       })
