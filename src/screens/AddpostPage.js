900b06d52b328f37476e505457eb21fd2e9a9430
abdom11 <abd123abdo.com@gmail.com>
2023-02-28 19:32:54 +0300

    ss

--- src/screens/AddpostPage.js
+++ src/screens/AddpostPage.js
@@ -44,6 +44,10 @@ const isRTL = I18nManager.isRTL;
 const windowHeight = Dimensions.get("window").height;
 const windowWidth = Dimensions.get("window").width;
 
+const customMapStyle = [];
+const QAlat = 25.300946829658887;
+const QAlon = 51.465748474001884;
+
 const AddPostPage = ({ navigation }) => {
   const user = users[0];
   const [content, setContent] = useState("");
@@ -55,18 +59,8 @@ const AddPostPage = ({ navigation }) => {
   const [isLoading, setIsLoading] = useState(false);
 
   const [isLost, setIsLost] = React.useState(false);
-  
-  const [region, setRegion] = useState({
-    latitude: 25.300946829658887,
-    latitudeDelta: 0.6631861591450701,
-    longitude: 51.465748474001884,
-    longitudeDelta: 0.3281260281801224,
-  });
-
 
   const onToggle = () => setIsLost(!isLost);
-
-  const [address, setAddress] = useState({});
 
   const mapViewRef = useRef(MapView);
 
@@ -168,10 +162,69 @@ const AddPostPage = ({ navigation }) => {
       createPost();
     }
   };
+  //----------------------------------------------------
+  const [markerPoint, setMarkerPoint] = useState({
+    latitude: 25.188497182423752,
+    longitude: 51.40093171969056,
+  });
+  const [currLocation, setCurrLocation] = useState(null);
+  const [address, setAddress] = useState(null);
+  const refMap = useRef(MapView);
+
+  const getAddressAsync = async (latitude, longitude) => {
+    let address = await Location.reverseGeocodeAsync({ latitude, longitude });
+    setAddress(address[0]);
+    return address;
+  };
+
+  const onSetLocation = async () => {
+    const location = {
+      latitude: currLocation.latitude,
+      longitude: currLocation.longitude,
+    };
+
+    refMap.current.animateCamera({
+      center: location,
+
+      // Only when using Google Maps.
+      zoom: 17,
+    });
+
+    await getAddressAsync(location.latitude, location.longitude);
+    // console.log(`${location.latitude}, ${location.longitude}`);
+    console.log(address);
+  };
+
+  const getAddressFromCoords = async (lat, lng) => {
+    try {
+      const response = await axios.get(
+        `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=AIzaSyBb2lef_VaN4m9OlvngArW3h84ul1DHZIo`
+      );
+      if (response.data.status === "OK") {
+        const { address_components } = response.data.results[0];
+        // find the street name component in the address_components array
+        const streetName = address_components.find((component) =>
+          component.types.includes("route")
+        ).long_name;
+        return streetName;
+      } else {
+        console.log(
+          "Geocode was not successful for the following reason: ",
+          response.data.status
+        );
+        return null;
+      }
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  };
 
   const onLocation = () => {
     setModalStatus(true);
-  };
+  }
+
+  //----------------------------------------------------
 
   return (
     <KeyboardAvoidingView
@@ -303,7 +356,58 @@ const AddPostPage = ({ navigation }) => {
         style={{ justifyContent: "center", alignItems: "center" }}
       >
         <View style={styles.modalLocation}>
-          <GetMap />
+          <View style={styles.container}>
+            <MapView
+              showsMyLocationButton={false}
+              showsUserLocation={true}
+              customMapStyle={customMapStyle}
+              onUserLocationChange={(e) =>
+                setCurrLocation(e.nativeEvent.coordinate)
+              } //here
+              ref={refMap}
+              style={{ height: "100%", width: "100%" }}
+              // onRegionChangeComplete={async (Region) => {
+              //   await getAddressAsync(Region.latitude, Region.longitude);
+              //   console.log(`${Region.latitude}, ${Region.longitude}`);
+
+              //   console.log(address);
+
+              //   await getAddressFromCoords(Region.latitude, Region.longitude);
+              // }}
+              initialRegion={{
+                latitude: QAlat,
+                latitudeDelta: 0.6631861591450701,
+                longitude: QAlon,
+                longitudeDelta: 0.3281260281801224,
+              }}
+              onPress={(e) => {
+                setMarkerPoint(e.nativeEvent.coordinate);
+              }}
+              // onRegionChangeComplete={setCoordinate}
+            ></MapView>
+            <View style={{ position: "absolute", top: 40, left: 0 }}>
+              <Button title={"to curr location"} onPress={onLocation} />
+            </View>
+            <Entypo
+              name={"location-pin"}
+              size={30}
+              color={"#660032"}
+              style={styles.pin}
+            />
+            <View
+              style={{
+                position: "absolute",
+                width: 6,
+                height: 6,
+                borderRadius: 0,
+                left: "50%",
+                top: "50%",
+              }}
+            />
+            <View style={{ position: "absolute", bottom: 0 }}>
+              <Text>{JSON.stringify(address)}</Text>
+            </View>
+          </View>
         </View>
       </Modal>
       {isLoading && (
